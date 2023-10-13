34ddcbd4b71bdd71b871236c11ab5c755eb6effc
AliAliAljasim <147882389+AliAliAljasim@users.noreply.github.com>
2023-10-13 17:26:16 -0400

    Delete Python Code/Ably Test trial.py

--- Python Code/Ably Test trial.py
+++
@@ -1,4 +0,0 @@
-async def main():
-    ably = AblyRealtime('xVLyHw.vW1ihQ:UoEJTcPU9I9bw0Gj6bt_YHHUn37T1aA8b-1mh1PV8-w')
-    await ably.connection.once_async('connected')
-    print("Connected to Ably")
